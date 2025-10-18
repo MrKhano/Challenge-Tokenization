82324eebaaeba4d8be48edb794666aaaa70b5dc5
Renaud Blaser <renaud_blaser@hotmail.com>
2025-10-18 18:48:28 +0200

    fix: minimal Next.js config for Vercel with Server Actions

--- packages/nextjs/next.config.mjs
+++ packages/nextjs/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: true, // active Server Actions
+  },
 };
 
 export default nextConfig;
